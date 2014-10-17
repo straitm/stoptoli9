d40b9b7a966c12a0e5512910f42fc69b567177ff
Matthew Strait <strait@hep.uchicago.edu>
2014-10-17 15:42:20 -0500

    asdf

---
+++ c9finalfit.C
@@ -0,0 +1,224 @@
+void c9finalfit(const int nncut = 3, const int nncuthigh = 5)
+{
+
+  TFile * fiel = new TFile("/cp/s4/strait/fullfido-100s-3-25MeV-20140925-earlymich.root", "read");
+  TTree * t = (TTree *) fiel->Get("t");
+
+  TCanvas * c = new TCanvas(Form("c%d", nncut), Form("c%d", nncut));
+//  c->Divide(2, 1);
+//  c->cd(1);
+
+  const char * const RED = "\033[31;1m"; // bold red
+  const char * const CLR = "\033[m"    ; // clear
+
+  const char * const ndef = "(latennear+ngdnear-latengdnear)";
+
+  const string scut =
+  Form("!earlymich && miche < 12 && dist < 400 && %s >= %d && %s <= %d && e > 4"
+    "&& e < 18 && timeleft > 100e3", ndef, nncut, ndef, nncuthigh);
+
+  const char * const cut = scut.c_str();
+
+  t->Draw(Form("dt/1000 >> hfit%d(10000, 0.001, 100)", nncut), cut);
+  TH1 * hfit = gROOT->FindObject(Form("hfit%d", nncut));
+
+  TF1 * ee = new TF1(Form("ee%d", nncut), "[0]*exp(-x*log(2)/0.0202) + "
+               "[1]*exp(-x*log(2)/[2]) + "
+               "[3]*exp(-x*log(2)/7.13) + "
+               "[4]", 0, 100);
+
+  ee->SetParameters(1, 1, 0.7700, 1, 1);
+  ee->FixParameter(3, 0);
+  ee->FixParameter(2, 0.1270);
+
+  ee->SetParLimits(1, 0, 10);
+  if(nncut >= 3){
+    ee->SetParLimits(0, 0, 10);
+    ee->SetParLimits(4, 0, 10);
+  }
+  int p0isfixed = 0;
+  if(nncut >= 4){
+    p0isfixed = 1;
+    ee->FixParameter(0, 0);
+    ee->FixParameter(4, 0);
+  }
+
+  hfit->Fit(Form("ee%d", nncut), "le");
+
+  if(ee->GetParameter(0) < 1e-6){
+    p0isfixed = 1;
+    ee->FixParameter(0, 0);
+    hfit->Fit(Form("ee%d", nncut), "le");
+  }
+
+  t->Draw(Form("dt/1000 >> hdisp%d(200, 0.001, 100.01)", nncut), cut, "hist");
+  TH1 * hdisp = gROOT->FindObject(Form("hdisp%d", nncut));
+  if(hdisp->GetBinContent(2) > 5) hdisp->Draw("e");
+
+  TF1 * eedisp = ee->Clone(Form("eedisp%d", nncut));
+  eedisp->SetNpx(400);
+  eedisp->SetLineColor(kRed);
+
+  int tomult[4] = { 0, 1, 3, 4};
+  const double mult = hdisp->GetBinWidth(1)/hfit->GetBinWidth(1);
+  for(int i = 0; i < 4; i++)
+    eedisp->SetParameter(tomult[i], eedisp->GetParameter(tomult[i])*mult);
+  eedisp->Draw("same");
+
+  TF1 * b12 = new TF1(Form("b12", nncut), "[0]*exp(-x*log(2)/0.0202)" , 0, 100);
+  TF1 * b8 = new TF1(Form("b8", nncut), "[0]*exp(-x*log(2)/[1])", 0, 100);
+  TF1 * acc = new TF1(Form("acc", nncut), "[0]", 0, 100);
+
+  b12->SetNpx(400);
+  b8->SetNpx(400);
+
+  TF1 * parts[3] = { b12, b8, acc };
+
+  b12->SetParameter(0, eedisp->GetParameter(0));
+  b8 ->SetParameter(0, eedisp->GetParameter(1));
+  b8 ->SetParameter(1, eedisp->GetParameter(2));
+  acc->SetParameter(0, eedisp->GetParameter(4));
+
+  for(int i = 0; i < 3; i++){
+    parts[i]->SetLineStyle(7);
+    parts[i]->SetLineWidth(2);
+    parts[i]->Draw("Same");
+  } 
+
+  const double Nfound = b8->Integral(0, 20)/hdisp->GetBinWidth(1);
+  double Nerrup, Nerrlo;
+  char * errtype = NULL;
+  if(hfit->GetEntries() < 3){
+    errtype = "HESSE";
+    Nerrup = Nfound * ee->GetParError(1)/ee->GetParameter(1);
+    Nerrlo = Nfound * ee->GetParError(1)/ee->GetParameter(1);
+  }
+  else{
+    errtype = "MINOS";
+    Nerrup = Nfound * gMinuit->fErp[1-p0isfixed]/ee->GetParameter(1);
+    Nerrlo = Nfound * gMinuit->fErn[1-p0isfixed]/ee->GetParameter(1);
+  }
+
+  printf("%sN found: %f +%f %f %s%s\n",
+         RED, Nfound, Nerrup, Nerrlo, errtype, CLR);
+
+  const double tp = 0.64,
+               gp = 0.93-0.0303, // since not accepting early nH
+               tedgep = 0.5449*tp+(1-0.5449)*gp,
+               gedgep = 0.499*gp;
+
+  // oxygen
+  const double targrate = 0.2,
+               targvesrate =  5*   85./(85.+58.),
+               targbitsrate = 5*(1-85./(85.+58.)),
+               gcrate = 0.3,
+               gcvesrate = 1.4; 
+/*
+  // nitrogen
+  const double targrate = 0.09,
+               targvesrate =  0,
+               targbitsrate = 0,
+               gcrate = 0.24-0.09,
+               gcvesrate = 0;*/
+
+  const double totalrate = gcvesrate+gcrate+targvesrate+targbitsrate+targrate;
+
+  const double targf    =              targrate/totalrate,
+               targedgef=           targvesrate/totalrate,
+               gcf      = (targbitsrate+gcrate)/totalrate,
+               gcedgef  =             gcvesrate/totalrate;
+
+
+  double tpneffs[6] = {
+      pow(tp,0)*pow(1-tp,5),
+    5*pow(tp,1)*pow(1-tp,4),
+   10*pow(tp,2)*pow(1-tp,3),
+   10*pow(tp,3)*pow(1-tp,2),
+    5*pow(tp,4)*pow(1-tp,1),
+      pow(tp,5)*pow(1-tp,0)
+  };
+
+  double tpedgeneffs[6] = {
+      pow(tedgep,0)*pow(1-tedgep,5),
+    5*pow(tedgep,1)*pow(1-tedgep,4),
+   10*pow(tedgep,2)*pow(1-tedgep,3),
+   10*pow(tedgep,3)*pow(1-tedgep,2),
+    5*pow(tedgep,4)*pow(1-tedgep,1),
+      pow(tedgep,5)*pow(1-tedgep,0)
+  };
+
+  double gpneffs[6] = {
+      pow(gp,0)*pow(1-gp,5),
+    5*pow(gp,1)*pow(1-gp,4),
+   10*pow(gp,2)*pow(1-gp,3),
+   10*pow(gp,3)*pow(1-gp,2),
+    5*pow(gp,4)*pow(1-gp,1),
+      pow(gp,5)*pow(1-gp,0)
+  };
+
+  double gpedgeneffs[6] = {
+      pow(gedgep,0)*pow(1-gedgep,5),
+    5*pow(gedgep,1)*pow(1-gedgep,4),
+   10*pow(gedgep,2)*pow(1-gedgep,3),
+   10*pow(gedgep,3)*pow(1-gedgep,2),
+    5*pow(gedgep,4)*pow(1-gedgep,1),
+      pow(gedgep,5)*pow(1-gedgep,0)
+  };
+
+  double neff = 0, tpneff = 0, tpedgeneff = 0, gpneff = 0, gpedgeneff = 0;
+  for(int i = nncut; i <= nncuthigh && i < 6; i++){
+    neff += tpneffs[i]*targf + tpedgeneffs[i]*targedgef 
+          + gpneffs[i]*gcf   + gpedgeneffs[i]*gcedgef;
+    tpneff += tpneffs[i];
+    tpedgeneff += tpedgeneffs[i];
+    gpneff += gpneffs[i];
+    gpedgeneff += gpedgeneffs[i];
+  }
+    
+
+  const double eff = 1
+    * exp(-1.*log(2)/127.00) // half-life and 1ms veto
+    * 0.981 // subsequent muons
+    * 0.977 // previous muons
+    * 0.948 // delta r
+    * 0.9709 // 100s from end of run
+    * 0.969 // energy
+    * neff
+  ;
+
+  const double captures = 358. * 489.509;
+
+  const double toprob = 1./captures/eff;
+
+  printf("Efficiencies: neutron, total: %.3f (%.3f, %.3f, %.3f, %.3f), %.3f\n",
+         neff, tpneff, tpedgeneff, gpneff, gpedgeneff, eff);
+
+  printf("%sProb: %g +%g %g%s\n", 
+      RED, toprob*Nfound, toprob*Nerrup, toprob*Nerrlo, CLR);
+
+/*  TF1 gaus("gaus", "gaus(0)", 0, 20);
+  gaus.SetParameters(1, toprob*Nfound, toprob*Nerrup);
+
+  for(int i = 1; i < 400; i++){
+    const double up = 0.01*i;
+    const double frac = gaus.Integral(0, up)/gaus.Integral(0, 20);
+    printf("%f %f\n", up, frac);
+    if(frac > 0.9){
+      printf("90%% limit = %f\n", up);
+      printf("90%% limit prob = %f\n", up*toprob);
+      printf("90%% limit prob *0.1/1.22 = %f\n", up*toprob *(1+0.1/1.22));
+      break;
+    }
+  } */
+
+/*
+  c->cd(2);
+
+  const string escut =
+    Form("!earlymich && miche < 12 && dist < 400 && %s >= %d && %s <= %d "
+    "&& timeleft > 100e3", ndef, nncut, ndef, nncuthigh);
+  const char * const ecut = escut.c_str();
+
+  t->Draw(Form("e >> ehist%d(250, 0, 25)", nncut), ecut, "e");
+*/
+}
--- n16finalfit.C
+++ n16finalfit.C
@@ -16,97 +16,146 @@
   const char * const CLR = "\033[m"    ; // clear
 
   const char * const scutwoe = "!earlymich && nnear == 0 && miche < 12 "
-    "&& dist < 400 &&  timeleft > 100e3";
+    "&& dist < 200 && ttlastvalid > 0.1 && ttlastmuon > 1 && timeleft > 100e3 ";
 
-  const char * const acrcut = "distfromacrylic(dx, dy, dz) < 100";
+  // Or not.
+  // Require that the muon be reconstructed to stop near the acrylic,
+  // then assume it actually stopped in the acrylic, find the point
+  // where it stopped if that is true, and find the distance from there
+  // to the decay, then require that distance be small. Ok, then?
+  //" && distfromacrylic(mx, my, mz) < 200";
+  const char * const acrcut = "1"; /*pow(dx - nearestax(mx, my, mz), 2) +"
+                              "pow(dy - nearestay(mx, my, mz), 2) +"
+                              "pow(dz - nearestaz(mx, my, mz), 2)"
+                              " < 200**2"; */
 
-  const char * const cute = "e > 5 && e < 12 && (e < 7.4 || e > 8.4)";
+  const char * const cute = "e > 5 && e < 11.5 && (e < 7.4 || e > 8.4)";
 
-  const char * const cutb12 = "b12like < 0.4";
+  const char * const cutb12 = "b12like < 0.1";
 
   const char * const cutx[3] = {
-   "dz > -(1229+200)",
-   "(dx**2 + dy**2 < (1150+200)**2 || "
-   "(atan2(dy,dx) > -1.7+3.14159/2 && atan2(dy,dx) < -1.7+3*3.14159/2))",
+   "mz > -1500",
+   "1",
+   //"(mx**2 + my**2 < 1400**2 || "
+   //"(atan2(my,mx) > -1.7+3.14159/2 && atan2(my,mx) < -1.7+3*3.14159/2))",
    "1"
   };
 
-  const char * const cutxx = (string(cutx[1]) + "&&" + string(cutx[0])).c_str(); 
+  const char * const cutxx = string(cutx[0]).c_str(); 
 
   const string scut = Form("%s && %s && %s && %s", scutwoe, cutb12, cute, acrcut);
 
-  const char * xscut = string(Form("%s && dt > 10e3 && %s && %s && %s", scutwoe, cute, cutb12, acrcut)).c_str();
+  const char * xscut = string(Form("%s && dt > 4e3 && dt < 30e3 && %s && %s && %s",
+                                   scutwoe, cute, cutb12, acrcut)).c_str();
 
-  c->cd(2)->cd(1);t->Draw("dz:dx**2+dy**2;r",xscut,".");
-  c->cd(2)->cd(2);t->Draw("dz:dx**2+dy**2",Form("%s&&%s",xscut,cutxx),".");
-  c->cd(2)->cd(3);t->Draw("dz:dy*sqrt(1708^2-dy^2)+1708^2*atan(dy/sqrt(1708^2-dy^2))",xscut,".");
-  c->cd(2)->cd(4);t->Draw("dz:dy*sqrt(1708^2-dy^2)+1708^2*atan(dy/sqrt(1708^2-dy^2))",Form("%s&&%s",xscut,cutxx),".");
-  c->cd(2)->cd(5);t->Draw("dz:dx*sqrt(1708^2-dx^2)+1708^2*atan(dx/sqrt(1708^2-dx^2))",xscut,".");
-  c->cd(2)->cd(6);t->Draw("dz:dx*sqrt(1708^2-dx^2)+1708^2*atan(dx/sqrt(1708^2-dx^2))",Form("%s&&%s",xscut,cutxx),".");
-  c->cd(2)->cd(7);t->Draw("dy:dx",xscut,".");
-  c->cd(2)->cd(8);t->Draw("dy:dx",Form("%s&&%s",xscut,cutxx),".");
+/*const char* drawx ="dx";
+  const char* drawy ="dy";
+  const char* drawz ="dz";
+  const char* drawsx="dx*sqrt(1708^2-dx^2)+1708^2*atan(dx/sqrt(1708^2-dx^2))";
+  const char* drawsy="dy*sqrt(1708^2-dy^2)+1708^2*atan(dy/sqrt(1708^2-dy^2))";
+  const char* drawr ="dx**2+dy**2"; */
+  const char* drawx ="mx";
+  const char* drawy ="my";
+  const char* drawz ="mz";
+  const char* drawsx="mx*sqrt(1708^2-mx^2)+1708^2*atan(mx/sqrt(1708^2-mx^2))";
+  const char* drawsy="my*sqrt(1708^2-my^2)+1708^2*atan(my/sqrt(1708^2-my^2))";
+  const char* drawr ="mx**2+my**2";
+
+/*
+  c->cd(2)->cd(1);t->Draw(Form("%s:%s",drawz,drawr),xscut,".");
+  c->cd(2)->cd(2);t->Draw(Form("%s:%s",drawz,drawr),Form("%s&&%s",xscut,cutxx),".");
+  c->cd(2)->cd(3);t->Draw(Form("%s:%s",drawz,drawsy),xscut,".");
+  c->cd(2)->cd(4);t->Draw(Form("%s:%s",drawz,drawsy),Form("%s&&%s",xscut,cutxx),".");
+  c->cd(2)->cd(5);t->Draw(Form("%s:%s",drawz,drawsx),xscut,".");
+  c->cd(2)->cd(6);t->Draw(Form("%s:%s",drawz,drawsx),Form("%s&&%s",xscut,cutxx),".");
+  c->cd(2)->cd(7);t->Draw(Form("%s:%s",drawy,drawx),xscut,".");
+  c->cd(2)->cd(8);t->Draw(Form("%s:%s",drawy,drawx),Form("%s&&%s",xscut,cutxx),".");
+*/
 
   c->cd(1)->cd(1)->SetLogy();
 
-  t->Draw("dt/1000 >> hfit(10000, 0.001, 100)", scut.c_str());
+  printf("Drawing...\n");
+  t->Draw("dt/1000 >> hfit(1000, 0.001, 100)", scut.c_str());
+
+ // This is the n16 efficiency.
+ // What is the be11 efficiency?
+  const double eff = 1
+    * 0.981 // subsequent muons
+    * 0.977 // previous muons
+    * 0.565 // delta r
+    * 0.9709 // 100s from end of run
+    * 0.8 // energy XXX
+    * 0.9 // spatial cuts XXX
+  ;
 
   TF1 * ee = new TF1("ee",
-               "[0]*exp(-x*log(2)/[1]) + "
-               "[2]*exp(-x*log(2)/0.0202) + "
-               "[3]*exp(-x*log(2)/0.8399) + "
-               "[4] +"
-               "[5]*exp(-x*log(2)/13.81)", 0, 100);
+               Form("%f*[0]/7.13*log(2)*exp(-x*log(2)/[1]) + " // n16
+               "[2]*exp(-x*log(2)/0.0202) + " // b12
+               "[3]*exp(-x*log(2)/0.8399) + " // li8
+               "[4] +" // accidentals
+               "%f*[5]/13.81*log(2)*exp(-x*log(2)/[6])", eff, eff), 0, 100); // be11
 
-  ee->SetParameters(1, 7.13, 1, 1, 1, 1);
+  ee->SetParameters(0.1, 7.13, 3000, 2, 0.1, 1);
   ee->FixParameter(0, 0);
   ee->FixParameter(1, 7.13);
+  ee->FixParameter(6, 13.81);
   ee->FixParameter(5, 0);
-  if(fit) hfit->Fit("ee", "l");  // fit with no n16 or be11
+  if(fit) printf("fitting...\n"), hfit->Fit("ee", "l");  // fit with no n16 or be11
 
   double likenon16 = 0; if(fit) likenon16 = gMinuit->fAmin;
   ee->ReleaseParameter(0);
-  if(fit) hfit->Fit("ee", "l"); // fit with n16
+  ee->ReleaseParameter(5);
+  ee->SetParLimits(0, 0, 100);
+  ee->SetParLimits(5, 0, 100);
+  if(fit) printf("fitting...\n"), hfit->Fit("ee", "l"); // fit with n16 and be11
 
   double likewn16 = 0; if(fit) likewn16 = gMinuit->fAmin;
 
   if(likenon16 > likewn16)
-    printf("%sSignificance %f%s\n", RED,
+    printf("%sSignificance for n16 and/or be11 rather than neither %f%s\n", RED,
            sqrt(2)*sqrt(likenon16-likewn16), CLR);
 
   ee->ReleaseParameter(1);
-  if(fit) hfit->Fit("ee", "le");  // fit with free lifetime n16
+  if(fit) printf("fitting...\n"), hfit->Fit("ee", "le");  // fit with free lifetime n16
 
   if(fit)
     printf("%sHalf life %f +%f %f%s\n", RED, ee->GetParameter(1),
            gMinuit->fErp[1], gMinuit->fErn[1], CLR);
 
   ee->FixParameter(1, 7.13);
-  ee->ReleaseParameter(5);
-  ee->SetParLimits(5, 0, 10);
 
-  if(fit) hfit->Fit("ee", "le");  // fit with b16 and be11
+  ee->ReleaseParameter(6);
+  ee->SetParLimits(6, 8, 100);
+  if(fit) printf("fitting...\n"), hfit->Fit("ee", "le");  // fit with free lifetime be11
 
-  t->Draw("dt/1000 >> hdisp(200, 0.001, 100)", scut.c_str(), "hist");
+  if(fit)
+    printf("%sHalf life %f +%f %f%s\n", RED, ee->GetParameter(6),
+           gMinuit->fErp[5], gMinuit->fErn[5], CLR);
+
+  ee->FixParameter(6, 13.81);
+
+
+  if(fit) printf("fitting...\n"), hfit->Fit("ee", "le");  // fit with b16 and be11
+
+  printf("Drawing...\n");
+  t->Draw("dt/1000 >> hdisp(49, 2, 100)", scut.c_str(), "hist");
   if(hdisp->GetBinContent(2) > 5) hdisp->Draw("e");
-  c->Modified();
-  c->Update();
 
   TF1 * eedisp = (TF1 *)ee->Clone("eedisp");
   eedisp->SetNpx(400);
   eedisp->SetLineColor(kRed);
 
-  bool atlimit[5] = {false, false, false, false, false};
+  bool nearlimit[5] = {false, false, false, false, false};
   int tomult[5] = { 0, 2, 3, 4, 5};
   const double mult = hdisp->GetBinWidth(1)/hfit->GetBinWidth(1);
   for(int i = 0; i < 5; i++){
-    double setit = eedisp->GetParameter(tomult[i]);
-    if(eedisp->GetParameter(tomult[i]) < eedisp->GetParError(tomult[i])){
-      setit += eedisp->GetParError(tomult[i]);
-      atlimit[i] = true;
-    }
+    if(eedisp->GetParameter(tomult[i]) < eedisp->GetParError(tomult[i]))
+      nearlimit[i] = true;
 
     eedisp->SetParameter(tomult[i], eedisp->GetParameter(tomult[i])*mult);
+    eedisp->SetParError( tomult[i], eedisp->GetParError (tomult[i])*mult);
   }
+
   eedisp->Draw("same");
   c->Update();
 
@@ -123,16 +172,29 @@
   TF1 * parts[5] = { b12, li8, n16, acc, be11 };
 
   n16->SetParameter(0, eedisp->GetParameter(0));
+  n16->SetParError(0, eedisp->GetParError(0));
   n16->SetParameter(1, eedisp->GetParameter(1));
+  n16->SetParError(1, eedisp->GetParError(1));
   b12->SetParameter(0, eedisp->GetParameter(2));
+  b12->SetParError(0, eedisp->GetParError(2));
   li8->SetParameter(0, eedisp->GetParameter(3));
+  li8->SetParError(0, eedisp->GetParError(3));
   acc->SetParameter(0, eedisp->GetParameter(4));
+  acc->SetParError(0, eedisp->GetParError(4));
   be11->SetParameter(0, eedisp->GetParameter(5));
+  be11->SetParError(0, eedisp->GetParError(5));
+
   
   for(int i = 0; i < 5; i++){
-    if(atlimit[i]) parts[i]->SetLineStyle(kDotted);
     parts[i]->SetLineStyle(7);
     parts[i]->SetLineWidth(2);
+    if(nearlimit[i]){
+      printf("plot %d (%d) is near limit\n", i, tomult[i]);
+      parts[i]->SetParameter(0, parts[i]->GetParameter(0)+
+                                parts[i]->GetParError(0));
+      parts[i]->SetLineStyle(kDotted);
+      parts[i]->SetLineWidth(1);
+    }
     parts[i]->Draw("Same");
   } 
 
@@ -146,21 +208,12 @@
   }
   else{
     errtype = "MINOS";
-    if(fit) Nerrup = Nfound * gMinuit.fErp[0]/ee->GetParameter(0);
-    if(fit) Nerrlo = Nfound * gMinuit.fErn[0]/ee->GetParameter(0);
+    if(fit) Nerrup = Nfound * gMinuit->fErp[0]/ee->GetParameter(0);
+    if(fit) Nerrlo = Nfound * gMinuit->fErn[0]/ee->GetParameter(0);
   }
 
-  printf("%sN found: %f +%f %f %s%s\n",
+  printf("%sN N-16 found: %f +%f %f %s%s\n",
          RED, Nfound, Nerrup, Nerrlo, errtype, CLR);
-
-  const double eff = 1
-    * 0.981 // subsequent muons
-    * 0.977 // previous muons
-    * 0.565 // delta r
-    * 0.9709 // 100s from end of run
-    * 0.8 // energy XXX
-    * 0.9 // spatial cuts XXX
-  ;
 
   const double captures = (7.5 + 7.0 + 0.2 + 0.3) * 489.509;
 
@@ -184,26 +237,39 @@
     }
   } */
 
-  c->cd(1)->cd(2)->cd(1);
+  c->cd(1)->cd(2)->cd(1)->SetLogy();
 
-  const string escut = Form("%s && %s && %s && %s && dt > 10e3",
+  const string escut = Form("%s && %s && %s && %s && dt > 4e3 && dt < 30e3",
                             scutwoe, cutxx, cutb12, acrcut);
-
-  t->Draw("e >> ehist(250, 0, 25)", escut.c_str(), "e");
+  printf("Drawing...\n");
+  t->Draw("e >> ehist(50, 4, 14)", escut.c_str(), "e");
 
   c->cd(1)->cd(2)->cd(2);
 
+  const string b12scut = Form("%s && %s && %s && %s && dt > 4e3 && dt < 30e3",
+                              scutwoe, cutxx, cute, acrcut);
 
-  const string b12scut = Form("%s && %s && %s && %s && dt > 10e3",
-                            scutwoe, cutxx, cute, acrcut);
+  printf("Drawing...\n");
+  t->Draw("b12like >> b12hist(500, 0, 1)", b12scut.c_str(), "e");
 
-  t->Draw("b12like >> b12hist(100, 0, 1)", b12scut.c_str(), "e");
+  gMinuit->Command("set print 0");
 
-  c->cd(1)->cd(2)->cd(3);
+  gMinuit->fUp = 2.3./2; // 90% in 1D
+  gMinuit->Command("mncont 1 6 100");
+  TGraph * ninty_1d = ((TGraph*)gMinuit->GetPlot())->Clone();
 
+  gMinuit->fUp = 4.61/2; // 90% CL contour in 2D
+  gMinuit->Command("mncont 1 6 100");
+  TGraph * ninty_2d = ((TGraph*)gMinuit->GetPlot())->Clone();
 
-  const string acrscut = Form("%s && %s && %s && %s && dt > 0e3",
-                            scutwoe, cutxx, cutb12, cute );
+  gMinuit->fUp = 11.83/2; // 99.73% contour in 2D
+  gMinuit->Command("MNC 1 6 99");
+  TGraph * ninty983_2d = ((TGraph*)gMinuit->GetPlot())->Clone();
 
-  t->Draw("distfromacrylic(dx, dy, dz) >> acrhist(42, 0, 1300)", acrscut.c_str(), "e");
+  new TCanvas;
+
+  ninty_1d->Draw("al");
+  ninty_2d->Draw("l");
+  ninty983_2d->Draw("l");
+
 }
