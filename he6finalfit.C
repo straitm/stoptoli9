d148f14bda549a417fbedee658f4f7f9b27ffe23
Matthew Strait <strait@hep.uchicago.edu>
2014-10-27 22:10:24 -0500

    he6finalfit.C

--- he6finalfit.C
+++ he6finalfit.C
@@ -10,9 +10,10 @@
 #include "stdio.h"
 
 double bgerr = 0;
-double abg[80], ali8[80], ahe6[80], asig[80];
+double abg[60], ali8[60], ahe6[60], asig[60];
 
-TH1D * ehistsig, * li8spec, * he6spec, * ehistbg;
+TH1D * ehistsig = NULL, * li8spec = NULL,
+     * he6spec = NULL,  * ehistbg = NULL;
 
 void fcn(int & npar, double * gin, double & like, double *par, int flag)
 {
@@ -21,7 +22,7 @@ void fcn(int & npar, double * gin, double & like, double *par, int flag)
   const double he6norm = par[2];
 
   like = 0;
-  for(int i = 7; i < 80; i++){
+  for(int i = 7; i < 60; i++){
     const double model = bgnorm*abg[i] + li8norm*ali8[i] + he6norm*ahe6[i];
     const double data = asig[i];
     like += model - data;
@@ -39,12 +40,13 @@ void he6finalfit()
   TFile fiel("/cp/s4/strait/fullfido-100s-1.5-25MeV-20141022.root");
   TTree * t = (TTree *) fiel.Get("t");
 
-  const double r2cut = 1150, zcut = 1229;
+  const double r2cut = 1708, zcut = 1786;
 
-  const char * const cut = string(Form(
-    "dx**2+dy**2 < 1150**2 && abs(dz) < 1229 &&"
-    "b12like < 0.4 && !earlymich && ttlastvalid > 0.1 && ttlastmuon > 1 && miche < 12 && dist < 400 && timeleft > 100e3", r2cut, zcut)).c_str();
-
+  char cut[1000];
+  snprintf(cut, 999, 
+    "dx**2+dy**2 < %f**2 && abs(dz) < %f &&"
+    "b12like < 0.4 && !earlymich && ttlastvalid > 0.1 && ttlastmuon > 1"
+    "&& miche < 12 && dist < 400 && timeleft > 100e3", r2cut, zcut);
 
   const double bglow = 7.13*5, bghigh = 100, siglow = 0.5, sighigh = 0.801*3;
   const double eff = 1
@@ -58,33 +60,59 @@ void he6finalfit()
     * (exp(-siglow*log(2)/0.801) - exp(-sighigh*log(2)/0.801))
   ;
 
+  const double li8eff = 1
+    * 0.981 // subsequent muons
+    * 0.977 // previous muons
+    * 0.948 // delta r
+    * 0.9709 // 100s from end of run
+    * 0.986 // ttlastvalid
+    * 0.96 // ttlastmuon
+    * 0.9994 // b12like
+    * (exp(-siglow*log(2)/0.8399) - exp(-sighigh*log(2)/0.8399))
+  ;
   TCanvas * c2 = new TCanvas;
+
+//#include "ehistbg.C"
+//#include "ehistsig.C"
  
+  if(!(ehistbg = (TH1D*)gROOT->FindObject("ehistbg"))){ 
+    ehistbg = new TH1D("ehistbg", "", 60, 0, 15);
+    fprintf(stderr, "regenerating ehistbg\n");
+    t->Draw("e >> ehistbg ", Form("%s && dt > %f", cut, bglow*1e3), "hist");
+
+    ehistbg->Scale((sighigh-siglow)/(bghigh-bglow));
+  }
+
+  if((bghigh-bglow)/15 > 1) bgerr *= sqrt((bghigh-bglow)/15); // XXX fudge
+  bgerr = 1/sqrt(ehistbg->Integral());
+
+  if(!(ehistsig = (TH1D*)gROOT->FindObject("ehistsig"))){ 
+    ehistsig = new TH1D("ehistsig", "", 60, 0, 15);
+    fprintf(stderr, "regenerating ehistsig\n");
+    t->Draw("e >> ehistsig", Form("%s && dt > %f && dt < %f", cut, siglow*1e3, sighigh*1e3), "esame");
+  }
+  
   TF1 betahe6("betahe6", "(x+0.511)*sqrt((x+0.511)**2 - 0.511**2) * (3.5076 - x)**2", 0, 3.51);
   TF1 betali8("betali8", "(x+0.511)*sqrt((x+0.511)**2 - 0.511**2) * (16.00517 - x)**2", 0, 16.00517);
   TF1 betan16l("betan16l", "(x+0.511)*sqrt((x+0.511)**2 - 0.511**2) * (10.4191 - x)**2", 0, 10.4191);
   TF1 betan16h1("betan16h1", "(x+0.511)*sqrt((x+0.511)**2 - 0.511**2) * (4.370 - x)**2", 0, 4.370);
   TF1 betan16h2("betan16h2", "(x+0.511)*sqrt((x+0.511)**2 - 0.511**2) * (3.3022- x)**2", 0,3.3022);
   TF1 betan16h3("betan16h3", "(x+0.511)*sqrt((x+0.511)**2 - 0.511**2) * (1.5472- x)**2", 0,1.5472);
-  he6spec = new TH1D("he6spec", "", 80, 0, 20);
-  li8spec = new TH1D("li8spec", "", 80, 0, 20);
-  for(int i = 0; i < 1e6; i++){ const double et=betahe6.GetRandom(); he6spec->Fill(gRandom->Gaus(et, sqrt(et)*0.077));}
-  for(int i = 0; i < 1e6; i++){ const double et=betali8.GetRandom(); li8spec->Fill(gRandom->Gaus(et, sqrt(et)*0.077));}
-
-
-  ehistbg = new TH1D("ehistbg", "", 80, 0, 20);
-  ehistsig = new TH1D("ehistsig", "", 80, 0, 20);
-  t->Draw("e >> ehistbg ", Form("%s && dt > %f", cut, bglow*1e3), "hist");
-  t->Draw("e >> ehistsig", Form("%s && dt > %f && dt < %f", cut, siglow*1e3, sighigh*1e3), "esame");
-  
-  bgerr = 1/sqrt(ehistbg->Integral());
-  if((bghigh-bglow)/20 > 1) bgerr *= sqrt((bghigh-bglow)/20); // XXX fudge
-  ehistbg->Scale((sighigh-siglow)/(bghigh-bglow));
+  he6spec = new TH1D("he6spec", "", 60, 0, 15);
+  li8spec = new TH1D("li8spec", "", 60, 0, 15);
+  for(int i = 0; i < 1e6; i++){
+    const double et=betahe6.GetRandom();
+    he6spec->Fill(gRandom->Gaus(et, sqrt(et)*0.077));
+  }
+  for(int i = 0; i < 1e6; i++){
+    const double et=betali8.GetRandom();
+    li8spec->Fill(gRandom->Gaus(et, sqrt(et)*0.077));
+  }
 
   he6spec->Scale(ehistsig->Integral()/he6spec->Integral());
   li8spec->Scale(ehistsig->Integral()/li8spec->Integral());
 
-  for(int i = 0; i < 80; i++){
+  for(int i = 0; i < 60; i++){
     abg[i]  =ehistbg ->GetBinContent(i+1);
     asig[i] =ehistsig->GetBinContent(i+1);
     ali8[i] = li8spec->GetBinContent(i+1);
@@ -99,11 +127,22 @@ void he6finalfit()
   mn->mnparm(2, "he6", 1, 0.01, 0, 10, err);
   printf("err? %d\n", err);
 
-  mn->Command("FIX 1");
+  mn->Command("SET PAR 3 0");
+  mn->Command("FIX 3");
+  mn->Command("MIGRAD");
+
+  const double chi2nohe6 = mn->fAmin;
+
+  mn->Command("RELEASE 3");
   mn->Command("MIGRAD");
   mn->Command("MINOS 10000 3");
-  mn->Command("RELEASE 1");
-  mn->Command("MINOS 10000 3");
+
+  const double chi2he6 = mn->fAmin;
+
+  if(chi2nohe6 - chi2he6 > 0)
+    printf("%sSignificance of He-6: %f%s\n", RED, sqrt(chi2nohe6 - chi2he6), CLR);
+
+  const double upforlim = 6.25 + chi2nohe6 - chi2he6;
 
   double dum, bgnorm, li8norm, he6norm;
   mn->GetParameter(0, bgnorm, dum);
@@ -113,18 +152,26 @@ void he6finalfit()
   double he6normerr;
   mn->mnerrs(2, dum, dum, he6normerr, dum);
 
+  double li8normerr;
+  mn->mnerrs(1, dum, dum, li8normerr, dum);
 
   ehistbg->Scale(bgnorm);
   li8spec->Scale(li8norm);
   he6spec->Scale(he6norm);
 
+  ehistsig->Draw("e");
+  ehistbg->Draw("same");
+  li8spec->Draw("same");
+  he6spec->Draw("same");
 
-  ehistsig->Draw();
-  ehistbg->Draw("esame");
-  li8spec->Draw("esame");
-  he6spec->Draw("esame");
+  TH1D * all = new TH1D("all", "", 60, 0, 15);
+  all->Add(ehistbg);
+  all->Add(li8spec);
+  all->Add(he6spec);
+  all->SetLineColor(kRed);
+  all->Draw("same");
 
-  const double captures = 139. * 489.509;
+  const double captures = 139. * 489.509 * pow(r2cut/1150., 2) * zcut/1229.;
 
   const double toprob = 1./captures/eff;
 
@@ -134,4 +181,32 @@ void he6finalfit()
   printf("%s efficiency: %f%s\n", RED, eff, CLR);
   printf("%s raw n He-6: %f +- %f%s\n", RED, nhe6, signhe6, CLR);
   printf("%s He-6 prob: %f +- %f%s\n", RED, nhe6*toprob, signhe6*toprob, CLR);
+
+  const double nli8 = li8spec->Integral();
+  const double signli8 = li8spec->Integral() * li8normerr/li8norm;
+
+  printf("%s Li-8 prob: %f +- %f%s\n", RED, nli8/captures/li8eff, signli8/captures/li8eff, CLR);
+
+  mn->fUp = upforlim;
+  mn->Command("MINOS 10000 3");
+ 
+  double he6up90;
+  mn->GetParameter(2, he6norm, dum);
+  mn->mnerrs(2, he6up90, dum, dum, dum);
+
+  printf("%s 90%% upper limit: %f+%f = %f%s\n", RED, he6norm, he6up90, he6norm+he6up90, CLR);
+  
+  TH1D * he6demo = (TH1D*) he6spec->Clone("he6demo");
+  he6demo->Scale((he6norm+he6up90)/he6norm);
+  he6demo->SetLineStyle(kDashed);
+  he6demo->Draw("same");
+
+  const double n90he6 = he6demo->Integral();
+
+  printf("%s 90%% upper limit raw n He-6: %f%s\n", RED, n90he6, CLR);
+  printf("%s 90%% upper limit He-6 prob: %f%s\n", RED, n90he6*toprob, CLR);
+
+  c2->SaveAs("he6.pdf");
+  c2->SaveAs("he6.C");
 }
+
