67909f93193447bded1643ed408186ca93c5a5bf
Matthew Strait <strait@hep.uchicago.edu>
2014-11-03 08:24:16 -0600

    he6finalfit.C

--- he6finalfit.C
+++ he6finalfit.C
@@ -12,26 +12,38 @@
 #include "stdio.h"
 
 double bgerr[4] = {0};
-double abg[4][120], asig[4][120], ali8[120], ahe6[120];
+double abg[4][120], asig[4][120], an16[4][120], ali8[120], ahe6[120];
 
 TH2D * ehistsig = NULL, * ehistbg = NULL;
-TH1D * li8spec = NULL, * he6spec = NULL;
+TH1D * li8spec = NULL, * he6spec = NULL, * n16spec[4] = {NULL};
 
 const double mus[4] = {0.45120, 0.44157, 0.49416, 2.2830};
+
+double bamacorrxy(const double xy, const double e)
+{
+  return xy * (0.970 + 0.013*e);
+}
+
+double bamacorrz(const double z, const double e)
+{
+  return z  * (0.985 + 0.005*e);
+}
 
 void fcn(int & npar, double * gin, double & like, double *par, int flag)
 {
   double mnbgnorm[4] = { par[0], par[1], par[2], par[3] };
   double mnli8norm = par[4];
   double mnhe6norm = par[5];
+  double mnn16norm[4] = { par[6], par[7], par[8], par[9] };
 
   like = 0;
   for(int j = 0; j < 4; j++){
     bool datahasstarted = false;
     for(int i = 6; i < 120; i++){
       double model = mnbgnorm[j]*abg[j][i]
-              + mnli8norm*mus[j]*ali8[i]
-              + mnhe6norm*mus[j]*ahe6[i];
+                   + mnn16norm[j]*an16[j][i]
+                   + mnli8norm*mus[j]*ali8[i]
+                   + mnhe6norm*mus[j]*ahe6[i];
       double data = asig[j][i];
       if(datahasstarted || data > 0) datahasstarted = true;
       else continue;
@@ -40,7 +52,9 @@ void fcn(int & npar, double * gin, double & like, double *par, int flag)
     } 
     like += pow((1-mnbgnorm[j])/bgerr[j], 2);
   }
-  like += pow((0.00229277 - mnli8norm)/1.79122e-04, 2);
+  // pull term for Li-8.  However, this doesn't let the Li-8
+  // part of the fit account for N-16, which is useful.
+  //like += pow((0.00229277 - mnli8norm)/1.79122e-04, 2);
   like *= 2;
 }
 
@@ -57,6 +71,8 @@ int classi(const double x, const double y, const double z)
 
 void he6finalfit()
 {
+  TH1::SetDefaultSumw2();
+
   const char * const RED     = "\033[31;1m"; // bold red
   const char * const CLR      = "\033[m"    ; // clear
 
@@ -143,13 +159,10 @@ void he6finalfit()
   }
   
   TF1 betahe6("betahe6", "(x+0.511)*sqrt((x+0.511)**2 - 0.511**2) * (3.5076 - x)**2", 0, 3.51);
-  TF1 betali8("betali8", "(x+0.511)*sqrt((x+0.511)**2 - 0.511**2) * (16.00517 - x)**2", 0, 16.00517);
-  TF1 betan16l("betan16l", "(x+0.511)*sqrt((x+0.511)**2 - 0.511**2) * (10.4191 - x)**2", 0, 10.4191);
-  TF1 betan16h1("betan16h1", "(x+0.511)*sqrt((x+0.511)**2 - 0.511**2) * (4.370 - x)**2", 0, 4.370);
-  TF1 betan16h2("betan16h2", "(x+0.511)*sqrt((x+0.511)**2 - 0.511**2) * (3.3022- x)**2", 0,3.3022);
-  TF1 betan16h3("betan16h3", "(x+0.511)*sqrt((x+0.511)**2 - 0.511**2) * (1.5472- x)**2", 0,1.5472);
   he6spec = new TH1D("he6spec", "", 120, 0, 15);
   li8spec = new TH1D("li8spec", "", 120, 0, 15);
+  for(int j = 0; j < 4; j++)
+    n16spec[j] = new TH1D(Form("n16spec%d", j), "", 120, 0, 15);
   for(int i = 0; i < 1e7; i++){
     const double et=betahe6.GetRandom();
     he6spec->Fill(gRandom->Gaus(et, sqrt(et*pow(0.077, 2)
@@ -159,10 +172,23 @@ void he6finalfit()
   
   TChain li8mc("data");
   li8mc.Add("/cp/s4/strait/li8mc/*.root");
-  li8mc.Draw("ctEvisID >> li8spec", "ctX[0]**2 + ctX[1]**2 < (1150*1.2)**2 && abs(ctX[2]) < 1229*1.2");
+  li8mc.Draw("ctEvisID >> li8spec", "bamacorrxy(ctX[0], ctEvisID)**2 + bamacorrxy(ctX[1], ctEvisID)**2 < (1150*1.2)**2 && abs(bamacorrz(ctX[2], ctEvisID)) < 1229*1.2");
+
+  TChain n16mc("data");
+  n16mc.Add("/cp/s4/strait/n16mc/*.root");
+  n16mc.Draw("ctEvisID >> n16spec0", "bamacorrxy(ctX[0], ctEvisID)**2 + bamacorrxy(ctX[1], ctEvisID)**2 < 800**2 && abs(bamacorrz(ctX[2], ctEvisID)) < 800");
+  n16mc.Draw("ctEvisID >> n16spec1", "bamacorrxy(ctX[0], ctEvisID)**2 + bamacorrxy(ctX[1], ctEvisID)**2 > 800**2 && "
+                                     "bamacorrxy(ctX[0], ctEvisID)**2 + bamacorrxy(ctX[1], ctEvisID)**2 < 1000**2 && "
+                                     "abs(bamacorrz(ctX[2], ctEvisID)) > 800 && abs(bamacorrz(ctX[2], ctEvisID)) < 1000");
+  n16mc.Draw("ctEvisID >> n16spec2", "bamacorrxy(ctX[0], ctEvisID)**2 + bamacorrxy(ctX[1], ctEvisID)**2 > 1000**2 && "
+                                     "bamacorrxy(ctX[0], ctEvisID)**2 + bamacorrxy(ctX[1], ctEvisID)**2 < 1150**2 && "
+                                     "abs(bamacorrz(ctX[2], ctEvisID)) > 800 && abs(bamacorrz(ctX[2], ctEvisID)) < 1229");
+  n16mc.Draw("ctEvisID >> n16spec3", "bamacorrxy(ctX[0], ctEvisID)**2 + bamacorrxy(ctX[1], ctEvisID)**2 > 1150**2 && abs(bamacorrz(ctX[2], ctEvisID)) > 1229");
 
   he6spec->Scale(ehistsig->Integral()/he6spec->Integral());
   li8spec->Scale(ehistsig->Integral()/li8spec->Integral());
+  for(int j = 0; j < 4; j++)
+    n16spec[j]->Scale(ehistsig->Integral()/n16spec[j]->Integral());
 
   for(int i = 0; i < 120; i++){
     ali8[i] = li8spec->GetBinContent(i+1);
@@ -170,23 +196,25 @@ void he6finalfit()
     for(int j = 0; j < 4; j++){
       abg[j][i]  =ehistbg ->GetBinContent(j+1, i+1);
       asig[j][i] =ehistsig->GetBinContent(j+1, i+1);
+      an16[j][i] = n16spec[j]->GetBinContent(i+1);
     }
   }
 
 
-  TMinuit * mn = new TMinuit(5);
+  TMinuit * mn = new TMinuit(9);
   mn->SetFCN(fcn);
   int err;
-  mn->mnparm(0, "bgtargin",  1, 0.01, 0,  0, err);
+  mn->mnparm(0, "bgtargin",   1, 0.01, 0,  0, err);
   mn->mnparm(1, "bgtargmid",  1, 0.01, 0,  0, err);
   mn->mnparm(2, "bgtargout",  1, 0.01, 0,  0, err);
-  mn->mnparm(3, "bggc",  1, 0.01, 0,  0, err);
-  mn->mnparm(4, "li8", 1, 0.001, 0.0001, 1, err);
-  mn->mnparm(5, "he6", 1, 0.01, 0, 10, err);
+  mn->mnparm(3, "bggc",       1, 0.01, 0,  0, err);
+  mn->mnparm(4, "li8",        1, 0.001, 0.0001, 1, err);
+  mn->mnparm(5, "he6",        1, 0.01, 0, 1, err);
+  mn->mnparm(6, "n16targin",  1, 0.001, 0, 0.1, err);
+  mn->mnparm(7, "n16targmid", 1, 0.001, 0, 0.1, err);
+  mn->mnparm(8, "n16targout", 1, 0.001, 0, 0.1, err);
+  mn->mnparm(9, "n16gc",      1, 0.001, 0, 0.1, err);
   printf("err? %d\n", err);
-
-//  mn->Command("SET PAR 5 0.00229277");
-//  mn->Command("FIX 5");
 
   mn->Command("SET PAR 6 0");
   mn->Command("FIX 6");
@@ -207,11 +235,12 @@ void he6finalfit()
 
   const double upforlim = 2.71 + chi2nohe6 - chi2he6;
 
-  double dum, bgnorm[4], li8norm, he6norm;
+  double dum, bgnorm[4], li8norm, he6norm, n16norm[4];
 
   for(int j = 0; j < 4; j++) mn->GetParameter(j, bgnorm[j], dum);
   mn->GetParameter(4, li8norm, dum);
   mn->GetParameter(5, he6norm, dum);
+  for(int j = 0; j < 4; j++) mn->GetParameter(j+6, n16norm[j], dum);
 
   double li8normerr, he6normerr;
   mn->mnerrs(4, dum, dum, li8normerr, dum);
@@ -250,6 +279,29 @@ void he6finalfit()
 
   printf("%s 90%% upper limit: %f+%f = %f%s\n", RED, he6norm, he6up90, he6norm+he6up90, CLR);
   
+  mn->Command("set print -3");
+  mn->Command("Fix 6");
+  double sump = 0;
+  for(int i =0;i<1000; i++){
+    mn->Command(Form("set  par 6 %f", i*0.01*he6norm));
+    mn->Command("Migrad");
+    double p = exp(-mn->fAmin);
+    sump += p;
+  }
+
+  double sump2 = 0;
+  int i;
+  for(i =0;i<1000; i++){
+    mn->Command(Form("set par 6 %f", i*0.01*he6norm));
+    mn->Command("Migrad");
+    double p = exp(-mn->fAmin);
+    sump2 += p;
+    if(sump2>sump*0.9){ printf("%s 90% bays lim: %f (best = %f)%s\n", RED, i*0.1*he6norm, he6norm, CLR); break;}
+  }
+  mn->Command("set print 0");
+
+  double he6norm90 = i*0.01*he6norm;
+
   double n90he6 = 0;
   for(int j = 1; j <= 4; j++){
     c2->cd(j);
@@ -258,10 +310,13 @@ void he6finalfit()
     TH1D * ehistsig_p=ehistsig->ProjectionY(Form("ehistsig_%d", j), j, j);
     TH1D * li8spec_p = (TH1D * )li8spec->Clone(Form("li8spec_%d", j));
     TH1D * he6spec_p = (TH1D * )he6spec->Clone(Form("he6spec_%d", j));
+    TH1D * n16spec_p = (TH1D * )n16spec[j-1]->Clone(Form("n16specp_%d", j));
 
     ehistbg_p->Scale(bgnorm[j-1]);
+    n16spec_p->Scale(n16norm[j-1]);
     li8spec_p->Scale(li8norm*mus[j-1]);
     he6spec_p->Scale(he6norm*mus[j-1]);
+
 
     ehistsig_p->SetLineColor(kRed);
     ehistsig_p->SetMarkerColor(kRed);
@@ -270,18 +325,26 @@ void he6finalfit()
 
     ehistbg_p->Draw("same");
 
+    n16spec_p->SetLineColor(kOrange);
+    n16spec_p->Draw("samehist");
+
     TH1D * bg_plus_li8 = (TH1D*)ehistbg_p->Clone(Form("bg_plus_li8_%d", j));
     bg_plus_li8->Add(li8spec_p);
     bg_plus_li8->SetLineColor(kBlue);
     bg_plus_li8->Draw("same");
 
-    TH1D * bg_plus_li8_plus_he6 = (TH1D*)bg_plus_li8->Clone(Form("bg_plus_li8_plus_he6_%d", j));
-    bg_plus_li8_plus_he6->Add(he6spec_p);
-    bg_plus_li8_plus_he6->SetLineColor(kGreen+2);
-    bg_plus_li8_plus_he6->Draw("same");
+    TH1D * bg_plus_li8_n16 = (TH1D*)bg_plus_li8->Clone(Form("bg_plus_li8_n16_%d", j));
+    bg_plus_li8_n16->Add(n16spec_p);
+    bg_plus_li8_n16->SetLineColor(kViolet);
+    bg_plus_li8_n16->Draw("same");
+
+    TH1D * bg_plus_li8_n16_he6 = (TH1D*)bg_plus_li8_n16->Clone(Form("bg_plus_li8_n16_he6_%d", j));
+    bg_plus_li8_n16_he6->Add(he6spec_p);
+    bg_plus_li8_n16_he6->SetLineColor(kGreen+2);
+    bg_plus_li8_n16_he6->Draw("same");
 
     TH1D * he6demo = (TH1D*) he6spec_p->Clone(Form("he6demo%d", j));
-    he6demo->Scale((he6norm+he6up90)/he6norm);
+    he6demo->Scale(he6norm90/he6norm);
     he6demo->SetLineStyle(kDashed);
     he6demo->Draw("same");
 
