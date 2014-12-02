2e89dd9e1b788ac44d2375bc8349142d1cb9393b
Matthew Strait <strait@hep.uchicago.edu>
2014-12-02 16:28:24 -0600

    Do b12 fit for several volumes

--- b12finalfit.C
+++ b12finalfit.C
@@ -92,15 +92,15 @@ void targ(TTree * t, TF1 * ee)
          rawintegral / eff / b12frac / livedays * (1-f12) * p13op12 * ferror3);
 }
 
-void innertarg(TTree * t, TF1 * ee)
+void innertarg(TTree * t, TF1 * ee, const double limr, const double limz)
 {
-  printf("\nIn the inner 750x750 target only:\n");
-  t->Draw("dt/1000 >> h(10000, 0.001, 100)",
-          "dx**2 + dy**2 < 750**2 && "
-          "abs(dz) < 750 &&"
+  printf("\nIn the inner %.1fx%.1f target only:\n", limr, limz);
+  t->Draw(Form("dt/1000 >> h%.0f(10000, 0.001, 100)", limr),
+          Form("dx**2 + dy**2 < %f**2 && "
+          "abs(dz) < %f &&"
           "timeleft > 100e3 && miche < 12 && "
-          "e > 4 && e < 14.5 && !earlymich", "e");
-  TH1D * h = (TH1D*)gROOT->FindObject("h");
+          "e > 4 && e < 14.5 && !earlymich", limr, limz), "e");
+  TH1D * h = (TH1D*)gROOT->FindObject(Form("h%.0f", limr));
   h->Fit("ee", "l");
   TF1 e("e", "[0]*exp(-x*log(2)/0.0202)", 0, 100);
   e.SetParameter(0, ee->GetParameter(0));
@@ -112,8 +112,9 @@ void innertarg(TTree * t, TF1 * ee)
   printf("b12 raw %f +- %f\n", rawintegral, ferrorfit * rawintegral);
   double ferror2 = sqrt(pow(ferrorfit, 2)+ pow(ferrorecut, 2)+
                         pow(ferror1213, 2)+ pow(ferrorb12, 2));
-  printfr("per day c12 cap rate %f +- %f\n",
+  printfr("per day c12 cap rate %f +- %f (stat) %f (tot)\n",
          rawintegral / eff / b12frac / livedays * f12,
+         rawintegral / eff / b12frac / livedays * f12 * ferrorfit,
          rawintegral / eff / b12frac / livedays * f12 * ferror2);
 
   double ferror3 = sqrt(pow(ferrorfit,  2)+ pow(ferrorecut, 2)+
@@ -136,7 +137,7 @@ void b12finalfit()
      "[4]*exp(-x*log(2)/[5]) + "
      "[6]*exp(-x*log(2)/[7]) + "
      "[8]", 0, 100);
-  ee->SetParLimits(1, 0, 30);
+  ee->SetParLimits(1, 0, 10);
 
   ee->SetParLimits(2, 0, 30);
   ee->SetParameter(3, 7.13);
@@ -153,6 +154,12 @@ void b12finalfit()
   ee->FixParameter(7, 0);
 
   all(t, ee);
+  ee->FixParameter(4, 0);
+  ee->FixParameter(5, 15);
   targ(t, ee);
-  innertarg(t, ee);
+  innertarg(t, ee, 1045, 1127);
+  ee->FixParameter(2, 0);
+  ee->FixParameter(3, 7.13);
+  innertarg(t, ee, 913, 985);
+  innertarg(t, ee, 724, 781);
 }
