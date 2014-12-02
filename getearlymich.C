2546ad312acf60e72d66f37c7775599a801761ae
Matthew Strait <strait@hep.uchicago.edu>
2014-12-02 16:28:42 -0600

    simplify getearlymich.C

--- getearlymich.C
+++ getearlymich.C
@@ -2,12 +2,9 @@
 #include "TFile.h"
 #include <iostream>
 
-void doit(const int run, const int mutrig, TTree * ts, TTree * tl)
+void doit(const int run, const int mutrig, TTree * ts)
 {
-  if(ts->GetEntries(Form("run == %d && mutrig == %d", run, mutrig)))
-    cout<<ts->GetEntries(Form("run==%d&&mutrig==%d&&earlymich",run,mutrig))<<endl;
-  else
-    cout<<tl->GetEntries(Form("run==%d&&mutrig==%d&&earlymich",run,mutrig))<<endl;
+  cout<<ts->GetEntries(Form("run==%d&&mutrig==%d&&earlymich",run,mutrig))<<endl;
 }
 
 void getearlymich()
@@ -15,18 +12,11 @@ void getearlymich()
   TFile fs("/cp/s4/strait/fullfido-100s-3-25MeV-20140925-earlymich.root", "read");
   TTree * ts = (TTree *)fs.Get("t"); 
 
-  TFile fl("/cp/s4/strait/fullfido-100s-3-25MeV-20140925-earlymich.root", "read");
-  TTree * tl = (TTree *)fs.Get("t"); 
-  
-  tl->SetBranchStatus("*", 0);
-  tl->SetBranchStatus("run", 1);
-  tl->SetBranchStatus("mutrig", 1);
-  tl->SetBranchStatus("earlymich", 1);
   ts->SetBranchStatus("*", 0);
   ts->SetBranchStatus("run", 1);
   ts->SetBranchStatus("mutrig", 1);
   ts->SetBranchStatus("earlymich", 1);
 
   int run, mutrig;
-  while(cin >> run >> mutrig) doit(run, mutrig, ts, tl);
+  while(cin >> run >> mutrig) doit(run, mutrig, ts);
 }
