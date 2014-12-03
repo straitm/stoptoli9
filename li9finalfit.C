9d4240d5886661e2f116d8b60bad5bd6e0358026
Matthew Strait <strait@hep.uchicago.edu>
2014-12-03 13:17:52 -0600

    li9finalfit: add c16 and n17 and pin things correctly.

--- li9finalfit.C
+++ li9finalfit.C
@@ -1,3 +1,17 @@
+void setupee2str(TF1 & ee2str, const double expectedgdfrac)
+{
+  ee2str.SetParLimits(1, 0, 1);
+  ee2str.FixParameter(1, expectedgdfrac);
+  ee2str.SetParLimits(2, 0, 1e-3);
+  ee2str.SetParLimits(3, 0, 1e-3);
+  ee2str.SetParLimits(4, 0, 20); // allow 100% production, plus
+                                // a factor of two for the O capture
+                                // normalization, plus a factor of ten
+                                // for other complications.
+  ee2str.SetParLimits(5, 0, 0.5);
+  ee2str.SetParLimits(6, 0, 1);
+}
+
 void li9finalfit(bool neutron = false)
 {
   const char * const RED     = "\033[31;1m"; // bold red
@@ -10,16 +24,20 @@ void li9finalfit(bool neutron = false)
   const double livetime = 489.509, nstop = 367.;
   const double nstoptarg = 139.0;
 
-  const double li9ebn = 0.5080, he8ebn = 0.1600, n17ebn = 0.951;
+  const double li9ebn = 0.5080,
+               he8ebn = 0.1600,
+               c16ebn = 0.99  * (5.6+0.2+0.3+0.7)*0.00205/nstop,
+               n17ebn = 0.951 * (5.6+0.2+0.3+0.7)*0.00205/nstop;
   const double distcuteff = (dist == 400?0.948:dist == 300?0.852:
                              dist == 200?0.565:dist==159?0.376:100000);
 
   // 30s begin-of-run requirement taken into account here
   const double denominator = 0.99127*livetime*nstop*distcuteff;
 
-  const double hcapfrac = 0.871;
+  const double gdcapfrac = 0.871;
 
-  /* DC3rdPub product of muon, light noise, OV, multiplicity, neutron (E, t, R), FV and IV efficiencies */
+  /* DC3rdPub product of muon, light noise, OV, multiplicity,
+     neutron (E, t, R), FV and IV efficiencies */
   const double Geff_sans_prompt_or_mun = (1-4.49/100.)*
          (1-0.01/100.)*
          (1-0.06/100.)*
@@ -28,7 +46,8 @@ void li9finalfit(bool neutron = false)
          (1-0.66/100.)*
          (1-0.04/100.);
 
-  const double Heff_sans_prompt_or_mun =   (1-1.25*4.49/100.)* // muon - ok, straightforwards scaling
+  const double Heff_sans_prompt_or_mun =
+         (1-1.25*4.49/100.)* // muon - ok, straightforwards scaling
          (1-0.01/100.)* // ? LN - same cut, but not obviously same eff
                         // however, *very* small for Gd, so...
          (1-0.06/100.)* // OV - ok, same
@@ -43,11 +62,14 @@ void li9finalfit(bool neutron = false)
   // First factor takes into account the efficiency of selecting a
   // neutron after a muon, second is the prompt energy cut, third as
   // documented above
-  const double Geff = (neutron?0.97*0.64:1)*0.996*Geff_sans_prompt_or_mun,
-               Heff = (neutron?0.97*0.93:1)*0.993*Heff_sans_prompt_or_mun;
+  const double Geff=(neutron?0.97*0.64:1)*0.996*Geff_sans_prompt_or_mun,
+               Heff=(neutron?0.97*0.93:1)*0.993*Heff_sans_prompt_or_mun;
+
+  const double expectedgdfrac = gdcapfrac*nstoptarg/nstop;
 
   char cut[1000];
-  snprintf(cut, 999, "%sdist < %f  && miche < 12 && !earlymich", dist, neutron?"n==1&&":" ");
+  snprintf(cut, 999, "%sdist < %f  && miche < 12 && !earlymich",
+           dist, neutron?"n==1&&":" ");
 
   ////////////////////////////////////////////////////////////////////
 
@@ -61,72 +83,73 @@ void li9finalfit(bool neutron = false)
 
   //////////////////
 
-  TF1 ee2str("ee2str",
-    Form("%f*%f*"
-    "(%f*(%f*[2]*(1-[1])/0.1783*log(2)*exp(-     x    *log(2)/0.1783) +" // H Li-9
-         "%f*[3]*(1-[1])/0.1191*log(2)*exp(-     x    *log(2)/0.1191) +" // H He-8
-         "%f*[4]*(1-[5])/4.173 *log(2)*exp(-     x    *log(2)/4.173 ) +" // H N-17
-          "[0]*(1-[1]))*(x < 30) + "                                    // H bg
-     "%f*(%f*  [2]*[1]  /0.1783*log(2)*exp(-(x-29.999)*log(2)/0.1783) +" // Gd Li-9
-         "%f*  [3]*[1]  /0.1191*log(2)*exp(-(x-29.999)*log(2)/0.1191) +" // Gd He-8
-         "%f*  [4]*[5]  /4.173 *log(2)*exp(-(x-29.999)*log(2)/4.173 ) +" // Gd N-17
-          "[0]*[1])*(x >=30))",                                         // Gd bg
-         h2fit->GetBinWidth(1), denominator, Heff, li9ebn, he8ebn, n17ebn, Geff, li9ebn, he8ebn, n17ebn), 0, 60);
-  ee2str.SetParameters(0.0001, 0.38, 0.0001, 0.00001, 0.00001, 0.1);
-  ee2str.SetParNames(  "bg",  "gdfrac", "li9",  "he8", "n17", "n17gdfrac");
+  TF1 ee2str("ee2str", Form("%f*%f*"
+    "(%f*(%f*[2]*(1-[1])/0.1783*log(2)*exp(-x*log(2)/0.1783)+" // H Li-9
+         "%f*[3]*(1-[1])/0.1191*log(2)*exp(-x*log(2)/0.1191)+" // H He-8
+         "%f*[4]*(1-[5])/4.173 *log(2)*exp(-x*log(2)/4.173 )+" // H N-17
+         "%f*[6]*(1-[5])/0.747 *log(2)*exp(-x*log(2)/0.747 )+" // H C-16
+          "[0]*(1-[1]))*(x < 30) + "                           // H bg
+     "%f*(%f*[2]*[1]/0.1783*log(2)*exp(-(x-29.999)*log(2)/0.1783)+"// Gd Li-9
+         "%f*[3]*[1]/0.1191*log(2)*exp(-(x-29.999)*log(2)/0.1191)+"// Gd He-8
+         "%f*[4]*[5]/4.173 *log(2)*exp(-(x-29.999)*log(2)/4.173 )+"// Gd N-17
+         "%f*[6]*[5]/0.747 *log(2)*exp(-(x-29.999)*log(2)/0.747 )+"// Gd C-16
+          "[0]*[1])*(x >=30))",                                    // Gd bg
+     h2fit->GetBinWidth(1), denominator, Heff, li9ebn, he8ebn, n17ebn,
+     Geff, li9ebn, he8ebn, n17ebn), 0, 60);
+  ee2str.SetParameters(1e-4, 0.38, 1e-4, 1e-5, 0.1,  0.1, 0.01, 1e-4);
+  ee2str.SetParNames(  "hbg", "gdfrac","li9", "he8","n17","ocapgdfrac",
+                     "c16");
   ee2str.SetNpx(400);
 
-  ee2str.SetParLimits(2, 0, 1e-3);
-//  ee2str.FixParameter(1,  165799./7778371 * 8.32142e-05); // XXX
-  ee2str.SetParLimits(1, 0, 1);
-  ee2str.SetParLimits(5, 0, 1);
-  ee2str.FixParameter(1, hcapfrac*nstoptarg/(nstop-hcapfrac*nstoptarg) * Geff/Heff);
-  ee2str.SetParLimits(3, 0, 1e-3);
-  ee2str.SetParLimits(4, 0, 1e-2);
+  setupee2str(ee2str, expectedgdfrac);
 
-  ee2str.FixParameter(3, 0);
-  h2fit->Fit("ee2str", "le", "", 0, 60);
-  printf("%sLi-9 prob without He-8: %f +%f %f%s\n", RED,
-    ee2str.GetParameter(2), gMinuit->fErp[2], gMinuit->fErn[2], CLR);
+  const unsigned int nfixforpureli9 = 4;
+  const double fixforpureli9[nfixforpureli9] = { 3, 4, 5, 6 };
 
-  ee2str.ReleaseParameter(3);
-  ee2str.SetParLimits(3, 0, 1e-3);
+  for(int i = 0; i < nfixforpureli9; i++)
+    ee2str.FixParameter(fixforpureli9[i], 0);
+
   h2fit->Fit("ee2str", "le", "", 0, 60);
-  printf("%sLi-9 prob with He-8: %f +%f %f%s\n", RED,
-    ee2str.GetParameter(2), gMinuit->fErp[2], gMinuit->fErn[2], CLR);
+  printf("%sLi-9 prob without other isotopes: %f +%f %f%s\n", RED,
+    ee2str.GetParameter(2), gMinuit->fErp[1], gMinuit->fErn[1], CLR);
+
+  for(int i = 0; i < nfixforpureli9; i++)
+    ee2str.ReleaseParameter(fixforpureli9[i]);
+  setupee2str(ee2str, expectedgdfrac);
+
+  h2fit->Fit("ee2str", "le", "", 0, 60);
+  printf("%sLi-9 prob with other isotopes: %f +%f %f%s\n", RED,
+    ee2str.GetParameter(2), gMinuit->fErp[1], gMinuit->fErn[1], CLR);
 
   TF1 * ee2str_save = (TF1 *)ee2str.Clone("ee2str_save");
 
   double minx = ee2str.GetParameter(2), miny = ee2str.GetParameter(3);
 
-  TCanvas * c3 = new TCanvas("c3", "c3", 600, 350);
+  //TCanvas * c3 = new TCanvas("c3", "c3", 600, 350);
   gMinuit->Command("Set print 0");
   gMinuit->Command("Set strategy 2");
 
   gMinuit->fUp = 1.0/2; // 68% in 1D
-  gMinuit->Command("mncont 3 4 100");
-  TGraph * sigma_1d = gMinuit->GetPlot()?(TGraph*)((TGraph*)gMinuit->GetPlot())->Clone():NULL;
+  gMinuit->Command("mncont 3 4 20");
+  TGraph * sigma_1d =
+    gMinuit->GetPlot()?(TGraph*)((TGraph*)gMinuit->GetPlot())->Clone():NULL;
 
   gMinuit->fUp = 2.3/2; // 90% in 1D
-//  gMinuit->Command("mncont 3 4 100");
-  TGraph * ninty_1d = gMinuit->GetPlot()?(TGraph*)((TGraph*)gMinuit->GetPlot())->Clone():NULL;
+  gMinuit->Command("mncont 3 4 20");
+  TGraph * ninty_1d =
+    gMinuit->GetPlot()?(TGraph*)((TGraph*)gMinuit->GetPlot())->Clone():NULL;
 
   gMinuit->fUp = 4.61/2; // 90%
-//  gMinuit->Command("mncont 3 4 100");
-  TGraph * ninty_2d = gMinuit->GetPlot()?(TGraph*)((TGraph*)gMinuit->GetPlot())->Clone():NULL;
+  gMinuit->Command("mncont 3 4 20");
+  TGraph * ninty_2d =
+    gMinuit->GetPlot()?(TGraph*)((TGraph*)gMinuit->GetPlot())->Clone():NULL;
 
-  gMinuit->fUp = 11.83/2; // 99.73%
-  gMinuit->Command("mncont 3 4 100");
-  TGraph * ninty973_2d = gMinuit->GetPlot()?(TGraph*)((TGraph*)gMinuit->GetPlot())->Clone():NULL;
-
-  if(ninty973_2d) ninty973_2d->SetFillColor(kViolet);
-  if(ninty973_2d) ninty973_2d->Draw("al");
-  if(ninty973_2d) ninty973_2d->GetXaxis()->SetRangeUser(0, 0.00075);
-  if(ninty973_2d) ninty973_2d->GetYaxis()->SetRangeUser(0, 0.001);
-  if(ninty973_2d) ((TGaxis*)(ninty973_2d->GetXaxis()))->SetMaxDigits(2);
-  if(ninty973_2d) ((TGaxis*)(ninty973_2d->GetYaxis()))->SetMaxDigits(2);
-  if(ninty_2d) ninty_2d->SetFillColor(kBlue);
-  if(ninty_2d) ninty_2d->Draw("l");
+  if(ninty_2d) ninty_2d->SetFillColor(kViolet);
+  if(ninty_2d) ninty_2d->Draw("alf");
+  if(ninty_2d) ninty_2d->GetXaxis()->SetRangeUser(0, 0.00079);
+  if(ninty_2d) ninty_2d->GetYaxis()->SetRangeUser(0, 0.001);
+  if(ninty_2d) ((TGaxis*)(ninty_2d->GetXaxis()))->SetMaxDigits(2);
+  if(ninty_2d) ((TGaxis*)(ninty_2d->GetYaxis()))->SetMaxDigits(2);
   if(ninty_1d) ninty_1d->SetLineColor(kRed);
   if(ninty_1d) ninty_1d->Draw("l");
   if(sigma_1d) sigma_1d->SetLineColor(kBlack);
@@ -141,44 +164,50 @@ void li9finalfit(bool neutron = false)
   c1->Divide(1, 3);
   c1->cd(1);
 
-  tg.Draw("dt/1000 >> hdispg(120, 0, 30)", cut);
-  th.Draw("dt/1000 >> hdisph(120, 0, 30)", cut);
+  tg.Draw("dt/1000 >> hdispg(60, 0, 30)", cut);
+  th.Draw("dt/1000 >> hdisph(60, 0, 30)", cut);
 
-  th.Draw("dt/1000 >> hdisp(120, 0, 30)", cut);
+  th.Draw("dt/1000 >> hdisp(60, 0, 30)", cut);
   tg.Draw("dt/1000 >> +hdisp", cut, "e");
 
   TF1 * eedisp = new TF1("eedisp",
     Form("%f*%f*"
-    "([6]*%f*(%f*[2]*(1-[1])/0.1783*log(2)*exp(-x*log(2)/0.1783) +"// H Li-9
+    "([7]*%f*(%f*[2]*(1-[1])/0.1783*log(2)*exp(-x*log(2)/0.1783) +"// H Li-9
          "%f*[3]*(1-[1])/0.1191*log(2)*exp(-x*log(2)/0.1191) +"    // H He-8
          "%f*[4]*(1-[5])/4.173 *log(2)*exp(-x*log(2)/4.173 ) +"    // H N-17
+         "%f*[6]*(1-[5])/0.747 *log(2)*exp(-x*log(2)/0.747 ) +"    // H C-16
           "[0]*(1-[1])) + "                                        // H bg
-     "[7]*%f*(%f*  [2]*[1]  /0.1783*log(2)*exp(-x*log(2)/0.1783) +"// Gd Li-9
+     "[8]*%f*(%f*  [2]*[1]  /0.1783*log(2)*exp(-x*log(2)/0.1783) +"// Gd Li-9
          "%f*  [3]*[1]  /0.1191*log(2)*exp(-x*log(2)/0.1191) +"    // Gd He-8
          "%f*  [4]*[5]  /4.173 *log(2)*exp(-x*log(2)/4.173 ) +"    // Gd N-17
+         "%f*  [6]*[5]  /0.747 *log(2)*exp(-x*log(2)/0.747 ) +"    // Gd C-16
           "[0]*[1]))",                                             // Gd bg
          h2fit->GetBinWidth(1), denominator, Heff, li9ebn, he8ebn,
          n17ebn, Geff, li9ebn, he8ebn, n17ebn), 0, 30);
   for(int i = 0; i < eedisp->GetNpar(); i++)
     eedisp->SetParameter(i, ee2str_save->GetParameter(i));
 
-  eedisp->SetParameter(6, hdisp->GetBinWidth(1)/h2fit->GetBinWidth(1));
-  eedisp->SetParameter(7, hdisp->GetBinWidth(1)/h2fit->GetBinWidth(1));
+  eedisp->SetParameter(ee2str->GetNpar(),
+                       hdisp->GetBinWidth(1)/h2fit->GetBinWidth(1));
+  eedisp->SetParameter(ee2str->GetNpar()+1,
+                       hdisp->GetBinWidth(1)/h2fit->GetBinWidth(1));
   eedisp->SetLineColor(kRed);
   eedisp->SetNpx(400);
   eedisp->Draw("Same");
 
   c1->cd(2);
   TF1 * eedisph = (TF1*)eedisp->Clone("eedisph");
-  eedisph->SetParameter(6, hdisp->GetBinWidth(1)/h2fit->GetBinWidth(1));
-  eedisph->SetParameter(7, 0);
+  eedisph->SetParameter(ee2str->GetNpar(),
+                        hdisp->GetBinWidth(1)/h2fit->GetBinWidth(1));
+  eedisph->SetParameter(ee2str->GetNpar()+1, 0);
   hdisph->Draw("e");
   eedisph->Draw("same");
   
   c1->cd(3);
   TF1 * eedispg = (TF1*)eedisp->Clone("eedispg");
-  eedispg->SetParameter(6, 0);
-  eedispg->SetParameter(7, hdisp->GetBinWidth(1)/h2fit->GetBinWidth(1));
+  eedispg->SetParameter(ee2str->GetNpar(), 0);
+  eedispg->SetParameter(ee2str->GetNpar()+1,
+                        hdisp->GetBinWidth(1)/h2fit->GetBinWidth(1));
   hdispg->Draw("e");
   eedispg->Draw("same");
 
