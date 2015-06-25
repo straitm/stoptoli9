cc4ae78e015e29f1c969cf7fd5af7038451e05b4
Matthew Strait <strait@hep.uchicago.edu>
2015-06-25 13:35:01 -0500

    Add more isotopes to my spectrum MC

--- b12spectrum.C
+++ b12spectrum.C
@@ -6,18 +6,18 @@
 TRandom3 r(0); // 0 -> UUID seed
 
 TH1D * b12spec = new TH1D("b12spec", "", 200, 0, 20);
+TH1D * be12spec = new TH1D("be12spec", "", 200, 0, 20);
 TH1D * b13spec = new TH1D("b13spec", "", 200, 0, 20);
 TH1D * b14spec = new TH1D("b14spec", "", 300, 0, 30);
 TH1D * li8spec = new TH1D("li8spec", "", 200, 0, 20);
 
 const int nb12b = 4;
-/*                                            forbidden?              allowed       allowed */
-double b12branchbe[nb12b] = {5.7150,                        5.7150,         8.9304, 13.3693};
-double b12branchge[nb12b] = {0.12*(13.3693-5.7150), 13.3693-5.7150, 13.3693-8.9304, 0};
-double tmp_b12branchp[nb12b] = {1.58*0.9, 1.58*0.1, 1.23, -1};
-double b12branchp[nb12b] = {tmp_b12branchp[0], tmp_b12branchp[0], tmp_b12branchp[0], 100 - tmp_b12branchp[0] - tmp_b12branchp[1]- tmp_b12branchp[2] };
-bool   b12brancha[nb12b] = {0, 0, 1, 1};
-double b12branch_aw[nb12b] = {0, 0, 0.0, 0.0093/0.511};
+const double b12q = 13.370;
+double b12branchbe[nb12b] = {b12q - 10.3, b12q - 7.6542, b12q - 4.4389, b12q};
+double b12branchge[nb12b] = {0.12*3.03,          7.6542,        4.4389, 0};
+double b12branchp[nb12b] = {0.0008,   0.015,         0.013,         0.9722};
+bool   b12brancha[nb12b] = {1, 1, 1, 1};
+double b12branch_aw[nb12b] = {0, 0, 0.0, 0}; // no idea
 
 const int nb13b = 8;
 double b13branchbe[nb13b] = {8.4909, 3.540, 4.577, 5.890, 9.834, 9.7527, 10.3478, 13.4372};
@@ -32,6 +32,16 @@ double li8branchge[nli8b] = { 0 };
 double li8branchp[nli8b] = { 1 };
 bool   li8brancha[nli8b] = { 1 };
 double li8branch_aw[nli8b] = { 0 };
+
+// It doesn't appear there have been measurements of the branching
+// probabilities.  This is a conservative table (for the purposes of
+// a one-sided energy cut).
+const int nbe12b = 1;
+double be12branchbe[nbe12b] = { 11.708 };
+double be12branchge[nbe12b] = { 0 };
+double be12branchp[nbe12b] = { 1 };
+bool   be12brancha[nbe12b] = { 1 };
+double be12branch_aw[nbe12b] = { 0 };
 
 const int nb14b = 4;
 const double b14q = 20.644;
@@ -124,6 +134,13 @@ void norm()
 
   {
     double sum = 0;
+    for(int i = 0; i < nbe12b; i++) sum += be12branchp[i];
+    for(int i = 0; i < nbe12b; i++) be12branchp[i] /= sum;
+    for(int i = 1; i < nbe12b; i++) be12branchp[i] += be12branchp[i-1];
+  }
+    
+  {
+    double sum = 0;
     for(int i = 0; i < nb12b; i++) sum += b12branchp[i];
     for(int i = 0; i < nb12b; i++) b12branchp[i] /= sum;
     for(int i = 1; i < nb12b; i++) b12branchp[i] += b12branchp[i-1];
@@ -142,6 +159,14 @@ void norm()
     for(int i = 0; i < nb14b; i++) b14branchp[i] /= sum;
     for(int i = 1; i < nb14b; i++) b14branchp[i] += b14branchp[i-1];
   }
+}
+
+
+void be12spectrum()
+{
+  norm();
+  fillit(be12spec, nbe12b, be12branchp, be12branchbe,
+         be12branchge,be12brancha, be12branch_aw, 1e6,1);
 }
 
 void b12spectrum()
