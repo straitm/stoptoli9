b533dd245f5fc0567248802fde36f66a4f388e20
Matthew Strait <strait@hep.uchicago.edu>
2015-09-24 15:05:43 -0500

    run2month.C

---
+++ run2month.C
@@ -0,0 +1,20 @@
+#include "TTree.h"
+#include "TFile.h"
+
+void run2month(const int run)
+{
+  TFile rf(Form("/cp/s4/dchooz/cheetah/prod-08-05_p01_v2/reduced.Run%07d_Seq010.root", run), "read");
+  TTree * data = (TTree*)rf.Get("data");
+
+  int date[6];
+  data->SetBranchAddress("date", date);
+  double trgtime;
+  data->SetBranchAddress("trgtime", &trgtime);
+  data->GetEntry(0);
+
+  printf("%04d%02d\n", date[0], date[1]);
+  t.tm_year = date[0]-1900;
+
+  delete data;
+  rf.Close();
+}
