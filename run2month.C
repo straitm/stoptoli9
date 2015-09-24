20c01b19217c2a8893ec0fd8052a9a6181a4a045
Matthew Strait <strait@hep.uchicago.edu>
2015-09-24 15:30:57 -0500

    Allow seq11 runs in getmonth.C

--- run2month.C
+++ run2month.C
@@ -4,7 +4,9 @@
 void run2month(const int run)
 {
   TFile rf(Form("/cp/s4/dchooz/cheetah/prod-08-05_p01_v2/reduced.Run%07d_Seq010.root", run), "read");
+  TFile rf11(Form("/cp/s4/dchooz/cheetah/seq11/reduced.Run%07d_Seq011.root", run), "read");
   TTree * data = (TTree*)rf.Get("data");
+  if(!data || data->IsZombie()) data = (TTree*)rf11.Get("data");
 
   int date[6];
   data->SetBranchAddress("date", date);
@@ -17,4 +19,5 @@ void run2month(const int run)
 
   delete data;
   rf.Close();
+  rf11.Close();
 }
