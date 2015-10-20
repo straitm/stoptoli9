1175884cd30f64c4e3e4513fb015ddb247338fd0
Matthew Strait <strait@hep.uchicago.edu>
2015-10-20 12:34:16 -0500

    interestingevents.sh

---
+++ interestingevents.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+(root -b /home/cp/strait/stoptoli9/seq1.ntuple.root << EOF
+
+t->Scan("mutrig", "dt > 0 && dt < 30 && e > 5 && e < 16 && fqiv < 200e3 && miche < 12 && abs(dz) > 10 && abs(dz) < 1700 && dx**2+dy**2 < 1700**2 && run == $1");
+
+EOF
+) | grep '^\* ' | awk '{print $4}' | grep '[1-9]'
