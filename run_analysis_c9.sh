8b6239d5c6891a2072404ec924bdca8d55eb3421
Matthew Strait <strait@hep.uchicago.edu>
2015-10-26 21:24:55 -0500

    rest of c9

--- run_analysis_c9.sh
+++ run_analysis_c9.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 (root -b -q c9finalfit.C"('o')";
-root -b -q c9finalfit.C"('n')") | tee c9_finanfit.out
+root -b -q c9finalfit.C"('n')") | tee c9_finalfit.out
 
 grep TECHNOTE c9_finalfit.out > c9_finalfit_out.technote
