814967ecb44684fff9d898e8a91a0cb1c76f659c
Matthew Strait <strait@hep.uchicago.edu>
2015-11-02 08:09:06 -0600

    mucount: return a ve

--- mucount_finalfit.C
+++ mucount_finalfit.C
@@ -1,21 +1,25 @@
 /* Meant to be run #included in other files */
 
-double mucountfinalfit_cut(const char * const cut,
-                           const bool returnerr = false)
+ve mucountfinalfit_cut(const char * const cut)
 {
+  printf("Counting muons...\n");
   TFile *_file0 = TFile::Open(rootfile3up, "read");
   TTree * t = (TTree *)_file0->Get("t");
 
   const int rawcount = t->GetEntries(cut);
   printf("Raw count: %d\n", rawcount);
 
-  const double val = rawcount*mum_frac*(1-mum_contamination);
-  const double err = sqrt(pow(rawcount*mum_frac_err,2)
-                         +pow(rawcount*mum_frac*mum_contamination_err,2));
+  ve answer;
+  answer.val = rawcount*mum_frac*(1-mum_contamination);
+  answer.err = sqrt(pow(rawcount*mum_frac_err,2)
+                   +pow(rawcount*mum_frac*mum_contamination_err,2));
   printf("Translated to mu- & corrected for contamination: %f +- %f\n",
-    val, err);
+    answer.val, answer.err);
 
-  return returnerr? err: val;
+  delete t;
+  delete _file0;
+
+  return answer;
 }
 
 void mucount_finalfit()
