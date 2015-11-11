5717f949b853fc17761981358348d4a535cb48a2
Matthew Strait <strait@hep.uchicago.edu>
2015-11-11 15:15:17 -0600

    Update near detector job script

--- ntuple_job_scripts/near-runb12job-3MeV.sh
+++ ntuple_job_scripts/near-runb12job-3MeV.sh
@@ -7,5 +7,5 @@ out=/cp/s4/strait/fullfido-near.d/$run
 mkdir -p $(dirname $out)
 
 if ( ! [ -e $out ] || file $out | grep -q empty) && ! [ -e $out.xz ]; then
-  ../b12search near 800 10000 3 25 /cp/s4/strait/jplighttree/SEQ12/data.$run.root /cp/s4/strait/ndfido_seq2/$run/$run.seq4.fido.root  > $out && root -b -q ../b12ntuple2root.C+'("'$out'", true)' && xz $out
+  ../b12search near 800 10000 3 25 /cp/s4/strait/jplighttree/SEQ13/data.$run.root /cp/s4/strait/ndfido/$run/$run.seq5.fido.root  > $out && root -b -q ../b12ntuple2root.C+'("'$out'", true)' && xz $out
 fi
