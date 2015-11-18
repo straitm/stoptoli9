1f749e7eb773f942347bbb82d4d4b013e87059d2
Matthew Strait <strait@hep.uchicago.edu>
2015-11-18 16:06:53 -0600

    Make all paths in b12arg lists NFS, and make b12 run scripts able to handle both cps4 and the older machines

--- ntuple_job_scripts/runb12job-0MeV.sh
+++ ntuple_job_scripts/runb12job-0MeV.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 n=$1
+shift
+. sr
 
 out=/cp/s4/strait/fullfido-100s-0-25MeV-20151117.d/$n
 
 mkdir -p $(dirname $out)
 
+if [ $(hostname) == cps4 ]; then
+  B12SEARCH=../b12search
+else
+  B12SEARCH=../b12search-SL5
+fi
+
+cd ~strait/stoptoli9/ntuple_job_scripts/
+
 if ! [ -e $out ] || file $out | grep -q empty; then
-  ../b12search far 400 100000 0 25 $(cat b12args$n) > $out && root -b -q ../b12ntuple2root.C+'("'$out'")' && gzip $out
+  $B12SEARCH far 400 100000 0 25 $(cat b12args$n) > $out && root -b -q ../b12ntuple2root.C+'("'$out'")' && gzip $out
 fi
--- ntuple_job_scripts/runb12job-3MeV.sh
+++ ntuple_job_scripts/runb12job-3MeV.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
 n=$1
+shift
+. sr
 
 out=/cp/s4/strait/fullfido-300s-3-25MeV-20151117.d/$n
 
 mkdir -p $(dirname $out)
 
+if [ $(hostname) == cps4 ]; then
+  B12SEARCH=../b12search
+  TOROOT=/home/cp/strait/stoptoli9/b12ntuple2root.C
+else
+  B12SEARCH=../b12search-SL5
+  TOROOT=/home/cp/strait/stoptoli9/b12ntuple2root_SL5.C
+fi
+
+cd ~strait/stoptoli9/ntuple_job_scripts/
+
 if ! [ -e $out ] || file $out | grep -q empty; then
-  ../b12search far 0 300000 3 25 $(cat b12args$n) > $out && root -b -q ../b12ntuple2root.C+'("'$out'")' && gzip $out
+  $B12SEARCH far 0 300000 3 25 $(cat b12args$n) > $out && root -b -q ${TOROOT}+'("'$out'")' && gzip $out
 fi
