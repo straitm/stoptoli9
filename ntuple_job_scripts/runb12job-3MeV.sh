6cd4867a9f3292398c5ab3776316260912cf6eef
Matthew Strait <strait@hep.uchicago.edu>
2015-11-18 10:11:07 -0600

    Add -1 option to xz in job scripts

--- ntuple_job_scripts/runb12job-3MeV.sh
+++ ntuple_job_scripts/runb12job-3MeV.sh
@@ -7,5 +7,5 @@ out=/cp/s4/strait/fullfido-300s-3-25MeV-20151117.d/$n
 mkdir -p $(dirname $out)
 
 if ! [ -e $out ] || file $out | grep -q empty; then
-  ../b12search far 0 300000 3 25 $(cat b12args$n) > $out && root -b -q ../b12ntuple2root.C+'("'$out'")' && xz $out
+  ../b12search far 0 300000 3 25 $(cat b12args$n) > $out && root -b -q ../b12ntuple2root.C+'("'$out'")' && xz -1 $out
 fi
