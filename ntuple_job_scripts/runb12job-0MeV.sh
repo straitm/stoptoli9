c060433d42e1575d63cb2fff5b16558dec96974d
Matthew Strait <strait@hep.uchicago.edu>
2015-11-18 10:29:41 -0600

    Switch to gzip in job scripts since it is twice as fast as xz -1 and gets nearly as good compression in this case

--- ntuple_job_scripts/runb12job-0MeV.sh
+++ ntuple_job_scripts/runb12job-0MeV.sh
@@ -7,5 +7,5 @@ out=/cp/s4/strait/fullfido-100s-0-25MeV-20151117.d/$n
 mkdir -p $(dirname $out)
 
 if ! [ -e $out ] || file $out | grep -q empty; then
-  ../b12search far 400 100000 0 25 $(cat b12args$n) > $out && root -b -q ../b12ntuple2root.C+'("'$out'")' && xz -1 $out
+  ../b12search far 400 100000 0 25 $(cat b12args$n) > $out && root -b -q ../b12ntuple2root.C+'("'$out'")' && gzip $out
 fi
