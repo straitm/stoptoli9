b853b82d90fc556a085e0ca656092554c3d58e89
Matthew Strait <strait@hep.uchicago.edu>
2015-11-18 08:41:44 -0600

    Update job scripts to remove obsolete b12ntuple2root parameter

--- ntuple_job_scripts/runb12job-0MeV.sh
+++ ntuple_job_scripts/runb12job-0MeV.sh
@@ -2,10 +2,10 @@
 
 n=$1
 
-out=/cp/s4/strait/fullfido-100s-0-25MeV-20151023.d/$n
+out=/cp/s4/strait/fullfido-100s-0-25MeV-20151117.d/$n
 
 mkdir -p $(dirname $out)
 
 if ! [ -e $out ] || file $out | grep -q empty; then
-  ../b12search far 400 100000 0 25 $(cat b12args$n) > $out && root -b -q ../b12ntuple2root.C+'("'$out'", true)' && xz $out
+  ../b12search far 400 100000 0 25 $(cat b12args$n) > $out && root -b -q ../b12ntuple2root.C+'("'$out'")' && xz $out
 fi
--- ntuple_job_scripts/runbe12job.sh
+++ ntuple_job_scripts/runbe12job.sh
@@ -2,10 +2,10 @@
 
 n=$1
 
-out=/cp/s4/strait/be12-20151102.d/$n
+out=/cp/s4/strait/be12-20151117.d/$n
 
 mkdir -p $(dirname $out)
 
 if ! [ -e $out ] || file $out | grep -q empty; then
-  ../be12search far 400 3000 3 25 $(cat b12args$n) > $out && root -b -q ../b12ntuple2root.C+'("'$out'", true)' && xz $out
+  ../be12search far 400 3000 3 25 $(cat b12args$n) > $out && root -b -q ../b12ntuple2root.C+'("'$out'")' && xz $out
 fi
