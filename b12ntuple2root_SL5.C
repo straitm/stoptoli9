d7e4e7836ba8567be3975ed8fecf5ba361442365
Matthew Strait <strait@hep.uchicago.edu>
2015-11-18 08:49:29 -0600

    Remove code duplication in b12ntuple2root scripts

--- b12ntuple2root_SL5.C
+++ b12ntuple2root_SL5.C
@@ -1,9 +1,4 @@
 #include "TTree.h"
 
 int b12ntuple2root_SL5(const char * fn)
-{
-  TTree t("t", "t");
-  if(!t.ReadFile(fn)) return 1;
-  t.SaveAs(Form("%s.root", fn));
-  return 0;
-}
+#include "b12ntuple2root.body"
