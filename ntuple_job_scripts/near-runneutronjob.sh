825938cd8e57ef891f736a362549f184297f58f6
Matthew Strait <strait@hep.uchicago.edu>
2015-11-24 20:00:50 -0600

    Add near neutron job script

--- ntuple_job_scripts/near-runneutronjob.sh
+++ ntuple_job_scripts/near-runneutronjob.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-n=$1
+run=$1
 
-if [ "$2" == post3rdpub ]; then
-  base=shortb12args-post3rdpub
-  suffix=post3rdpub
-else
-  base=shortb12args
-  suffix=
-fi
-
-out=/cp/s4/strait/fullfido-neutron-20150727.d/$n.$suffix
+out=/cp/s4/strait/fullfido-near-neutron.d/neutron-$run
 
 mkdir -p $(dirname $out)
 
 while ! [ -e $out ] || file $out | grep -q empty; do
-  ../neutronsearch far 0 0 3 25 $(cat $base$n) > $out
+  ../neutronsearch near 0 0 3 25 /cp/s4/strait/jplighttree/SEQ13/data.$run.root /cp/s4/strait/ndfido/$run/$run.seq5.fido.root  > $out && root -b -q ../b12ntuple2root.C+'("'$out'")' && xz $out
 done
